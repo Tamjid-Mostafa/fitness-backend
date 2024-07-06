fe32b441f70aabd3c575dffa7e4dfd21c91c6fc3
Tamjid Mostafa <tamjid430@gmail.com>
2024-07-06 10:52:51 +0600

    added author bio

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,39 @@
 import mongoose, { Document, Schema } from "mongoose";
 
+interface IProfilePicture {
+  url: string;
+  alt: string;
+  width: string;
+  height: string;
+}
+
 export interface IUser extends Document {
   name: string;
+  bio: string;
   email: string;
   password: string;
   role: string;
   provider: string;
   username: string;
   isVerified: boolean;
-  age?: String;
+  age?: string;
   gender?: string;
-  weight?: String;
-  height?: String;
+  weight?: string;
+  height?: string;
   goals?: string;
+  profilePicture?: IProfilePicture;
 }
+
+const ProfilePictureSchema: Schema = new Schema({
+  url: { type: String, required: true },
+  alt: { type: String, required: true },
+  width: { type: String, required: true },
+  height: { type: String, required: true },
+});
 
 const UserSchema: Schema = new Schema({
   name: { type: String, required: true },
+  bio: { type: String },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   role: { type: String, required: true, default: "member" },
@@ -28,6 +45,7 @@ const UserSchema: Schema = new Schema({
   weight: { type: String },
   height: { type: String },
   goals: { type: String },
+  profilePicture: { type: ProfilePictureSchema },
 });
 
 export default mongoose.model<IUser>("User", UserSchema);
